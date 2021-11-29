4a13aedef75f77b9fd3619e8b94d0457337ef68c
Douglas-san-anton <73114495+Douglas-san-anton@users.noreply.github.com>
2021-11-29 15:18:34 -0300

    Cambios de nombres

--- frontend/src/Components/ListItems.jsx
+++ frontend/src/Components/ListItems.jsx
@@ -22,25 +22,25 @@ export const mainListItems = (
       <ListItemIcon>
         <ShoppingCartIcon />
       </ListItemIcon>
-      <ListItemText primary="Orders" />
+      <ListItemText primary="Miembros" />
     </ListItem>
     <ListItem button>
       <ListItemIcon>
         <PeopleIcon />
       </ListItemIcon>
-      <ListItemText primary="Customers" />
+      <ListItemText primary="Equipos" />
     </ListItem>
     <ListItem button>
       <ListItemIcon>
         <BarChartIcon />
       </ListItemIcon>
-      <ListItemText primary="Reports" />
+      <ListItemText primary="TL" />
     </ListItem>
     <ListItem button>
       <ListItemIcon>
         <LayersIcon />
       </ListItemIcon>
-      <ListItemText primary="Integrations" />
+      <ListItemText primary="Proyectos" />
     </ListItem>
   </div>
 );
