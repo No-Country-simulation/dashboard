80ad010df61ccf1e8505bb73fa909516cb4034fc
Douglas-san-anton <73114495+Douglas-san-anton@users.noreply.github.com>
2021-11-30 09:49:15 -0300

    Creacion de utils

--- frontend/src/pages/Members.jsx
+++ frontend/src/pages/Members.jsx
@@ -3,10 +3,10 @@ import axios from "axios";
 import DataTable from "../Components/DataTable";
 
 export const Members = () => {
-  // const [member, setMember] = useState("");
+  const [members, setMembers] = useState("");
   const getMembers = async () => {
     const res = await axios.get("http://localhost:5000/api/members");
-    console.log(res.data.getAllMembers);
+    setMembers(res.data.getAllMembers);
   };
 
   useEffect(() => {
