56f7e78f5edf03e1eab7f14dfd9229009eadfc9e
Douglas-san-anton <73114495+Douglas-san-anton@users.noreply.github.com>
2021-12-01 15:22:47 -0300

    Members andando

--- frontend/src/pages/Members.jsx
+++ frontend/src/pages/Members.jsx
@@ -1,21 +1,22 @@
-// import React, { useEffect } from "react";
-// import axios from "axios";
-// import DataTable from "../Components/DataTable";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import DataTable from "../Components/DataTable";
 
-// // export const Members = () => {
-// //   // const [members, setMembers] = useState("");
-// //   const getMembers = async () => {
-// //     // const res = await axios.get("http://localhost:5000/api/members");
-// //     // setMembers(res.data.getAllMembers);
-// //   };
+export const Members = () => {
+  const [members, setMembers] = useState("");
+  const getMembers = async () => {
+    const res = await axios.get("http://localhost:5000/api/members");
+    setMembers(res.data.getAllMembers);
+    console.log(members);
+  };
 
-//   useEffect(() => {
-//     getMembers();
-//   }, []);
-//   return (
-//     <div>
-//       <h1>Members</h1>
-//       <DataTable />
-//     </div>
-//   );
-// };
+  useEffect(() => {
+    getMembers();
+  }, []);
+  return (
+    <div>
+      <h1>Members</h1>
+      <DataTable />
+    </div>
+  );
+};
